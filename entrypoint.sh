2ee65827e4adbcbcb67d2c243530d072e2032800
tseimandi <tom.seimandi@insee.fr>
2022-05-06 12:07:16 +0000

    Remove createsuperuser command in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 python3 manage.py migrate
 python3 manage.py collectstatic
-DJANGO_SUPERUSER_PASSWORD=$ADMIN_PASSWORD python3 manage.py createsuperuser \
-    --no-input \
-    --username=$ADMIN_USER \
-    --email=innovation@insee.fr
 gunicorn main.wsgi --bind 0.0.0.0:8000 -w 4
