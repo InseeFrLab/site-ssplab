a4a2fc6a91080e9a0fcb39d69756070bbebb915d
tseimandi <tom.seimandi@insee.fr>
2022-05-06 11:40:44 +0000

    Change image pull policy to Always for testing

--- site_ssplab/main/settings.py
+++ site_ssplab/main/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/var/www/ssplab.lab.sspcloud.fr/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
